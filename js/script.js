31e4f0eac224d1bfd218176dad617222affbadc0
DiaPrograkot <popelenko200290@gmail.com>
2024-09-06 23:47:50 +0300

    Gjgsnrf 15

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-// Используем async функцию для корректной загрузки
+import checkProxy from 'proxy-check'
+const proxies = await (
+  await fetch(
+    'https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all'
+  )
+).text()
+console.log(
+  await Promise.any(
+    proxies
+      .split('\r\n')
+      .slice(0, 99)
+      .map(addr => {
+        const [host, port] = addr.split(':')
+        return checkProxy({host, port}).then(() => addr)
+      })
+  )
+)// Используем async функцию для корректной загрузки
 (async () => {
   try {
     // Импортируем библиотеку Trystero
